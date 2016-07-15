3035c313545df211a16a7a30f5e33608e0630094
Rick-TIY <rick@theironyard.com>
2016-07-15 07:16:45 -0600

    Day 3 and 4 Stuff

--- src/main/java/toolbox/RandomGenerator.java
+++ src/main/java/toolbox/RandomGenerator.java
@@ -4,7 +4,16 @@ import java.util.Random;
 
 public class RandomGenerator {
 
+    private static Random random = new Random(System.currentTimeMillis());
+
+    // Just a random number
+    public static int randomInt() {
+        return random.nextInt();
+    }
+
+    // A random number between two numbers
     public static int randomInt(int min, int max) {
-        return 0; // DELETE ME
+        int randomNumber = random.nextInt((max - min) +1) +min;
+        return randomNumber;
     }
 }
