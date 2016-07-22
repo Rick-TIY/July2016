e0335174be05982e1088e41a1a29c4a346c7152d
Rick-TIY <rick@theironyard.com>
2016-07-22 12:52:37 -0600

    Checking in code for week 3 instruction

--- src/main/java/week2/day4/Day4InfiniteLoop.java
+++ src/main/java/week2/day4/Day4InfiniteLoop.java
@@ -1,6 +1,22 @@
 package week2.day4;
 
 public class Day4InfiniteLoop {
-
+    public static void main(String[] args) {
+        boolean shouldLoopEnd = false;
+        int targetNumber = (int) (Math.random() * 100);
+        System.out.println ("The target number is: " + targetNumber);
+        while (true) {
+            int guessNumber = (int) (Math.random() * 100);
+            System.out.println ("The loop guesses: " + guessNumber);
+            if (guessNumber == targetNumber) {
+                shouldLoopEnd = true;
+            }
+            if (shouldLoopEnd) {
+                System.out.println ("Woo hoo! We have a match!");
+            } else {
+                System.out.println ("Let's try again..");
+            }
+        }
+    }
 }
 
